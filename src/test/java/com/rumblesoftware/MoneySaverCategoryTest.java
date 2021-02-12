efb332dbec04cb1dc4c1afaab495988f18688e1e
cleiton <cleiton.ribeiro.dev@gmail.com>
2021-02-12 01:29:41 +0000

    Repository Layer Tests

--- src/test/java/com/rumblesoftware/MoneySaverCategoryTest.java
+++ src/test/java/com/rumblesoftware/MoneySaverCategoryTest.java
@@ -7,12 +7,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.context.ActiveProfiles;
 
+import com.rumblesoftware.cat.controller.CategoryControllerTests;
 import com.rumblesoftware.cat.service.CategoryServiceTests;
 
 @RunWith(Suite.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
-@Suite.SuiteClasses(value = { CategoryServiceTests.class })
+@Suite.SuiteClasses(value = { CategoryServiceTests.class, CategoryControllerTests.class})
 public class MoneySaverCategoryTest {
 
 	@Test
