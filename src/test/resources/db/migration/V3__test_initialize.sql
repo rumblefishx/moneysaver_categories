b6c9a8545571a6a711d1a9fd77813f317893a408
cleiton <cleiton.ribeiro.dev@gmail.com>
2021-02-16 00:16:36 +0000

    Auditory fields + Dockerfile

--- src/test/resources/db/migration/V3__test_initialize.sql
+++ src/test/resources/db/migration/V3__test_initialize.sql
@@ -1,1 +1,2 @@
-insert into tcategory (category_description, category_name, category_id, customer_id) values('category description example','name example',1000,1); 
+insert into tcategory (category_description, category_name, category_id, customer_id) values('category description example','name_example',1000,1);
+insert into tcategory (category_description, category_name, category_id, customer_id) values('category description example','category_name',1001,1);
